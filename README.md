75a94aed4b7d1ada6f222d023f97e69c69c96e2c
Carlos Salguero <cfsalguero@gmail.com>
2014-12-21 00:34:41 -0300

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ supervisorg
 ===========
 
 A small replacement for Python's Supervisord in Go
+
+This is a personal use project to have a Supervisord like program
+to use in my own Docker containers.
+
+Help welcome.
---
+++ config/config.go
@@ -0,0 +1,104 @@
+package config
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+)
+
+type Program struct {
+	Name      string
+	Command   string
+	Directory string
+	Priority  int64
+}
+
+func (p *Program) isValid() bool {
+	if p.Name != "" && p.Command != "" {
+		return true
+	}
+	return false
+}
+
+type Config struct {
+	Programs map[string]*Program
+}
+
+func (c *Config) addProgram(p *Program) {
+	if p != nil && p.isValid() {
+		c.Programs[p.Name] = p
+	}
+}
+
+func NewConfig(f io.Reader) (*Config, error) {
+	programSectionPrefix := "[program:"
+	config := &Config{
+		Programs: make(map[string]*Program),
+	}
+
+	r := bufio.NewReader(f)
+	inProgram := false
+	var program *Program = nil
+	lineNum := 0
+
+	for {
+		line, err := r.ReadString('\n')
+		if err != nil {
+			if inProgram {
+				config.addProgram(program)
+			}
+			break
+		}
+
+		lineNum += 1
+		line = strings.TrimSpace(line)
+
+		if line == "" {
+			if inProgram {
+				config.addProgram(program)
+			}
+			inProgram = false
+			continue
+		}
+
+		if strings.HasPrefix(line, programSectionPrefix) {
+			name := strings.TrimSuffix(strings.TrimPrefix(line, programSectionPrefix), "]")
+			inProgram = true
+			program = &Program{
+				Name: name,
+			}
+			continue
+		}
+
+		if inProgram {
+			e := parseLine(line, program)
+			if e != nil {
+				// TODO log the error
+			}
+		}
+	}
+	return config, nil
+}
+
+func parseLine(line string, program *Program) (err error) {
+	parts := strings.Split(line, "=")
+	if len(parts) != 2 {
+		return fmt.Errorf("Invalid program entry: %s", line)
+	}
+
+	key := strings.TrimSpace(strings.ToLower(parts[0]))
+	value := strings.TrimSpace(parts[1])
+
+	if key == "command" {
+		program.Command = value
+	}
+	if key == "directory" {
+		program.Directory = value
+	}
+	if key == "priority" {
+		program.Priority, err = strconv.ParseInt(value, 10, 64)
+	}
+	return err
+}
