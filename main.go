0be8a86811befa4bb2a7edcb4670958575312bc5
Carlos Salguero <cfsalguero@gmail.com>
2014-12-21 18:42:15 -0300

    Config file cli param

--- main.go
+++ main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/cfsalguero/supervisorg/config"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
+	var configFile string
 	configFiles := []string{"/etc/supervisorg/supervisor.conf", "supervisor.conf"}
+
+	flag.StringVar(&configFile, "configuration", "", "Default: "+strings.Join(configFiles, ", "))
+	flag.StringVar(&configFile, "c", "", "Default: "+strings.Join(configFiles, ", "))
+	flag.Parse()
+
+	if configFile != "" {
+		configFiles = []string{configFile}
+	}
+
 	f, err := chooseConfiFile(configFiles)
 	if err != nil {
 		log.Println(err.Error())
 		os.Exit(1)
 	}
+
 	conf, err := config.NewConfig(f)
 	for name, program := range conf.Programs {
-		fmt.Printf("Name: %s, Config: %+v\n", name, program)
+		cmd, err := program.GetCmd()
+		if err == nil {
+			fmt.Printf("Name: %s, Config: %+v\n", name, cmd)
+		}
 	}
 }
 
